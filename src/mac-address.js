09f6f083e6e7d01bcd9e7c6dde22c26c52e0f050
Наташка Овечкина <natallia.ovechkina@gmail.com>
2024-12-18 20:29:28 +0300

    feat: add solution of matrix-elements-sum

--- src/mac-address.js
+++ src/mac-address.js
@@ -15,8 +15,6 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function isMAC48Address(n) {
-  
-  
   return /^([0-9A-Fa-f]{2}-){5}([0-9A-Fa-f]{2})$/.test(n);
 }
 module.exports = {
--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  for (let i = 0; i < matrix[0].length; i++) {
+    let k = 0;
+    while (k < matrix.length && matrix[k][i] !== 0) {
+      sum += matrix[k][i];
+      k += 1;
+    }
+  }
+  return sum;
 }
 
 module.exports = {
